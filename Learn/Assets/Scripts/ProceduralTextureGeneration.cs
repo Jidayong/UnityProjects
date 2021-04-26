3eb2a13b50451e23871da40cd81b7fa7492d8486
Jidayong <jdy803@126.com>
2021-04-26 16:04:28 +0800

    add glass reflection effect

---
+++ Learn/Assets/Scripts/ProceduralTextureGeneration.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[ExecuteInEditMode]
+public class ProceduralTextureGeneration : MonoBehaviour
+{
+    public Material _material = null;
+
+    #region Material properties
+    [SerializeField, SetProperty("textureWidth")]
+    private int _textureWidth = 512;
+    public int textureWidth
+    {
+        get { return _textureWidth; }
+        set
+        {
+            _textureWidth = value;
+            _UpdateMaterial();
+        }
+    }
+
+    [SerializeField, SetProperty("backgroundColor")]
+    private Color _backgroundColor;
+
+    public Color backgrroundColor
+    {
+        get { return _backgroundColor; }
+        set 
+        {
+            _backgroundColor = value;
+            _UpdateMaterial();
+        }
+    }
+
+    [SerializeField, SetProperty("circleColor")]
+    private Color _circleColor;
+
+    public Color circleColor
+    {
+        get { return _circleColor; }
+        set
+        {
+            _circleColor = value;
+            _UpdateMaterial();
+        }
+    }
+
+    [SerializeField, SetProperty("blurFactor")]
+    private float _blurFactor = 2.0f;
+    public float blurFactor
+    {
+        get { return _blurFactor; }
+        set
+        {
+            _blurFactor = value;
+            _UpdateMaterial();
+        }
+    }
+    #endregion
+
+    private Texture2D _generatedTexture = null;
+
+    void Start()
+    {
+        if(_material == null)
+        {
+            Renderer renderer = gameObject.GetComponent<Renderer>();
+            if(renderer == null)
+            {
+                Debug.LogWarning("Can not find a renderer");
+                return;
+            }
+            _material = renderer.sharedMaterial;
+        }
+
+        _UpdateMaterial();
+    }
+
+    private void _UpdateMaterial()
+    {
+        if(_material != null)
+        {
+            _generatedTexture = _GenerateProceduralTexture();
+            _material.SetTexture("_MainTex", _generatedTexture);
+        }
+    }
+
+    private Color _MixColor(Color color0, Color color1, float mixFactor)
+    {
+        Color mixColor = Color.white;
+        mixColor.r = Mathf.Lerp(color0.r, color1.r, mixFactor);
+        mixColor.g = Mathf.Lerp(color0.g, color1.g, mixFactor);
+        mixColor.b = Mathf.Lerp(color0.b, color1.b, mixFactor);
+        return mixColor;
+    }
+
+    private Texture2D _GenerateProceduralTexture()
+    {
+        Texture2D proceduralTexture = new Texture2D(textureWidth, textureWidth);
+
+        float circleInterval = textureWidth / 4.0f;
+        float radius = textureWidth / 10.0f;
+        float edgeBlur = 1.0f / blurFactor;
+
+        for (int w = 0; w < textureWidth; w++)
+        {
+            for(int h = 0; h < textureWidth;  h++)
+            {
+                Color pixel = backgrroundColor;
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        Vector2 circleCenter = new Vector2(circleInterval * (i + 1), circleInterval * (j + 1));
+                        float dist = Vector2.Distance(new Vector2(w, h), circleCenter) - radius;
+
+                        Color color = _MixColor(circleColor, new Color(pixel.r, pixel.g, pixel.b, 1.0f), Mathf.SmoothStep(0f, 1.0f, dist * edgeBlur));
+                        pixel = _MixColor(pixel, color, color.a);
+                    }
+                }
+                proceduralTexture.SetPixel(w, h, pixel);
+            }
+        }
+        proceduralTexture.Apply();
+        return proceduralTexture;
+    }
+}
---
+++ Learn/Assets/Shaders/GlassRefraction/GlassRefraction.shader
@@ -0,0 +1,113 @@
+﻿Shader "Unlit/GlassRefraction"
+{
+    Properties
+    {
+		_MainTex("Main Tex", 2D) = "white" {}
+		_BumpMap("Normal Map", 2D) = "bump" {}
+		_Cubemap("Environment Cubemap", Cube) = "_Skybox" {}
+		_Distortion("Distortion", Range(0, 100)) = 10
+		_RefractAmount("Refract Amount", Range(0.0, 1.0)) = 1.0
+    }
+
+    SubShader
+    {
+		Tags { "Queue" = "Transparent" "RenderType" = "Opaque" }
+
+		GrabPass { "_RefractionTex" }
+        Pass
+        {
+			Tags
+			{
+				"LightMode"="ForwardBase"
+			}
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct a2v
+            {
+                float4 vertex : POSITION;
+				float4 texcoord : TEXCOORD0;
+				float3 normal : NORMAL;
+				float4 tangent : TANGENT;
+            };
+
+            struct v2f
+            {
+                float4 pos : SV_POSITION;
+				float4 scrPos : TEXCOORD0;
+				float3 worldPos : TEXCOORD1;
+				float4 uv : TEXCOORD2;
+
+				float3 worldTangent : TEXCOORD3;
+				float3 worldBinormal : TEXCOORD4;
+				float3 worldNormal : TEXCOORD5;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+
+			float _RefractAmount;
+			float _Distortion;
+
+			samplerCUBE _Cubemap;
+
+			sampler2D _BumpMap;
+			float4 _BumpMap_ST;
+
+			sampler2D _RefractionTex;
+			float4 _RefractionTex_TexelSize;
+
+            v2f vert (a2v v)
+            {
+                v2f o;
+                o.pos = UnityObjectToClipPos(v.vertex);
+				o.scrPos = ComputeGrabScreenPos(o.pos);							// 顶点在屏幕空间的坐标,注意该坐标是在齐次空间空间下的坐标，xy的范围为[0, w],因此在计算uv时，需要使用scrPos.xy / scrPos.w;
+				
+				o.worldPos = UnityObjectToWorldDir(v.vertex.xyz);				
+				o.uv.xy = TRANSFORM_TEX(v.texcoord, _MainTex);
+				o.uv.zw = TRANSFORM_TEX(v.texcoord, _BumpMap);
+
+				o.worldNormal = normalize(UnityObjectToWorldNormal(v.normal));
+				o.worldTangent = normalize(UnityObjectToWorldDir(v.tangent.xyz));
+				o.worldBinormal = normalize(cross(o.worldNormal, o.worldTangent) * v.tangent.w);
+                return o;
+            }
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				float3 worldViewDir = normalize(UnityWorldSpaceViewDir(i.worldPos));
+
+				// 切线空间下的法线方向
+				float3 bump = UnpackNormal(tex2D(_BumpMap, i.uv.zw));
+
+				// 根据像素的法线方向，求得折射的偏移
+				float2 offset = bump.xy * _Distortion * _RefractionTex_TexelSize.xy;
+				i.scrPos.xy = offset + i.scrPos.xy;
+				fixed3 refrCol = tex2D(_RefractionTex, i.scrPos.xy / i.scrPos.w).rgb;
+				
+				// 将切线空间下的法线变换到世界空间下，结算反射方向，进而通过环境球，得到反射color
+				float3x3 rot = float3x3(
+					i.worldTangent.x, i.worldBinormal.x, i.worldNormal.x,
+					i.worldTangent.y, i.worldBinormal.y, i.worldNormal.y,
+					i.worldTangent.z, i.worldBinormal.z, i.worldNormal.z);
+				
+				float3 reflDir = reflect(-worldViewDir, mul(rot, bump));
+				fixed4 texColor = tex2D(_MainTex, i.uv.xy);
+				
+				// 反射
+				fixed3 reflCol = texCUBE(_Cubemap, reflDir).rgb * texColor.rgb;
+
+				fixed3 color = lerp(reflCol, refrCol, _RefractAmount);
+
+				return fixed4(color.rgb, 1.0);
+            }
+            ENDCG
+        }
+    }
+	FallBack "Diffuse"
+}
